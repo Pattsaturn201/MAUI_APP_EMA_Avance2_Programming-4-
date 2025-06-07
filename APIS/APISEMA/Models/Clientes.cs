cda0fbe65f6bfc67fa26a9b5e75b8858fab1dec4
Edwin Saa <edwin.saa@udla.edu.ec>
2025-06-06 20:24:07 -0500

    Creacion y adaptacion de modelos a API

---
+++ APIS/APISEMA/Models/Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APISEMA.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string NombreCompleto { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string ApellidoCompleto { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        [Phone]
+        public string Telefono { get; set; }
+        [StringLength(200)]
+        public string Direccion { get; set; }
+        [Required]
+        public string MetodoPago { get; set; }
+
+    }
+}
